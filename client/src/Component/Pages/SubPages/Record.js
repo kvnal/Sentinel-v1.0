88cce4a6ed8a783effb11b943bcb4708367a9622
kvnal <72142540+kvnal@users.noreply.github.com>
2021-10-30 16:14:10 +0530

    changes

--- client/src/Component/Pages/SubPages/Record.js
+++ client/src/Component/Pages/SubPages/Record.js
@@ -6,7 +6,7 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 // const PROXY = "http://localhost:4000/api"
 // https://avatars.dicebear.com/api/croodles/your-custom-seed.svg?b=%23fafafa
 const API_DP = "https://avatars.dicebear.com/api/personas/"
-const Records = ({admin  ="false"}) => {
+const Records = ({admin  =false}) => {
     const [recordData, setRecordData] = useState(null);
     const [search, setSearch] = useState("");
 
@@ -25,9 +25,10 @@ const Records = ({admin  ="false"}) => {
         <div>
             <div>
                 <input className="text-black" value={search} type="text" placeholder = "search" onChange={(e)=>setSearch(e.target.value)}/>
+                { admin &&
                 <Link to="/admin/create/record">
                 <button  className="bg-sentinel-l2-g-blue text-sentinel-body-blue font-medium px-4 py-1 rounded-md">+ New Entry</button>
-                </Link>
+                </Link>}
             </div>
 
             <div className="mb-4">
@@ -48,8 +49,9 @@ const Records = ({admin  ="false"}) => {
                 })
                 .map(Element =>(
                     <Link key={Element._id} to={`/record/${Element._id}?q=${admin}`}>
-                    <div className="w-full  pb-3 rounded-xl text-senti nel-body-blue bg-sentinel-l2-g-blue p-2" >
-                        <span className="text-md text-left block">{relationType(Element.relation,Element.address)}</span>
+                    <div className={Element.status===1 ? "w-full  pb-3 rounded-xl text-sentinel-body-blue bg-red-400  p-2" : "w-full  pb-3 rounded-xl text-sentinel-body-blue bg-sentinel-l2-g-blue  p-2"} >
+                    {/* <div className="w-full  pb-3 rounded-xl p-2 bg-sentinel-l3-g-blue" > */}
+                        <span className="text-md text-left block font-bold">{relationType(Element.relation,Element.address)}</span>
                         <div >
                             <img className="rounded-xl"  src={`${API_DP}/${Element.name}.svg?b=%23fafafa`} alt="" />
                         </div>
@@ -59,7 +61,7 @@ const Records = ({admin  ="false"}) => {
                         <span className="text-md text-left block">Aadhaar : {Element.aadhaar}</span>
                         }
                         {Element.status===1 && 
-                        <span className="text-md text-left block">BANNED</span>
+                        <span  className="text-md text-left font-semibold text-white rounded-full rounded-ful bg-sentinel-l3-g-blue  px-2">BANNED</span>
                         }
 
                         </div>
